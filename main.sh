363afec32f84af52b869fb3f7a9c16a292e5ee74
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-02-25 15:31:39 +0300

    add github builder

---
+++ main.sh
@@ -0,0 +1,21 @@
+# Add dependent repositories
+wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
+add-apt-repository https://ppa.pika-os.com
+add-apt-repository ppa:pikaos/pika
+add-apt-repository ppa:kubuntu-ppa/backports
+# Clone Upstream
+git clone https://gitlab.freedesktop.org/wlroots/wlroots -b 0.16
+cp -rvf ./debian ./wlroots
+cd ./wlroots
+
+# Get build deps
+apt-get build-dep ./ -y
+
+# Build package
+LOGNAME=root dh_make --createorig -y -l -p wlroots_0.16
+dpkg-buildpackage
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
